ea302e375328e8aa1de56cb53f2fde6eac7df080
TanviThanekar <58777849+TanviThanekar@users.noreply.github.com>
2021-01-31 17:31:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
+from sklearn.externals import joblib
+import pickle
+import joblib
+
+
+app = Flask(__name__)
+m1 = open('term3_model.pkl','rb')
+model = joblib.load(m1)
+
+
+@app.route('/')
+def home():
+    #return 'Hello World'
+    return render_template('home.html')
+    #return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
+
+    #output = round(prediction[0], 2)
+    return render_template('home.html', prediction_text="Patient Results :{}".format(prediction[0]))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
